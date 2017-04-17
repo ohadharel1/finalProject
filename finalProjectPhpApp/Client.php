02d1bb245de22e65b9490f0b97c96efe2fb7b9c2
ohad <ohadharel1@gmail.com>
2017-04-17 13:31:21 +0300

    ohad: js module example for socketio. works with python

--- finalProjectPhpApp/Client.php
+++ finalProjectPhpApp/Client.php
@@ -1,34 +1,34 @@
 <?php
-	
+
 	class Client
 	{
-		private $host = "132.74.211.175";
+		private $host = "10.0.0.13";
 		private $port = 10002;
 		private $socket = null;
-		
+
 		function __construct()
 		{
 			$this->socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 			socket_connect($this->socket, $this->host, $this->port) or die("Could not connect to server\n");
 		}
-		
+
 		function send_msg($dict)
 		{
 			$msg = json_encode($dict, $assoc = true);
 			socket_write($this->socket, $msg, strlen($msg)) or die("Could not send data to server\n");
 		}
-		
+
 		function recv_msg()
 		{
 			$res = socket_read($this->socket, 1024) or die("Could not read server response\n");
 			$dict = json_decode($res, $assoc = true);
 			return $dict;
 		}
-		
+
 		function close()
 		{
 			socket_close($this->$socket);
 		}
 	}
-	
+
 ?>
