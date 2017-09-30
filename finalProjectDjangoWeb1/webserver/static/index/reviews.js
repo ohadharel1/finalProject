1f7c4be6ebb64b3f691e21198db8b2aa30c427cd
Nofar <nofarmos@gmail.com>
2017-09-30 14:22:57 +0300

    report page

--- finalProjectDjangoWeb1/webserver/static/index/reviews.js
+++ finalProjectDjangoWeb1/webserver/static/index/reviews.js
@@ -169,3 +169,14 @@ function showReportPopUpForReview(drone_num) {
             }
        });
    }
+
+
+/*   function myFunction(){
+  var x = document.getElementById('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+
+}*/
