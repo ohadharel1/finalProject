1d8c7c8c6e1593647befbf392c2a4e2e954cf86f
ohad <ohadharel1@gmail.com>
2017-10-11 15:38:39 +0300

    add dropdown btn for reports

--- finalProjectDjangoWeb1/webserver/static/index/reviews.js
+++ finalProjectDjangoWeb1/webserver/static/index/reviews.js
@@ -485,5 +485,20 @@ function saveErrorsPerMonthtImage() {
 }
 
 function myFunction() {
+console.log('clicked!')
     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
